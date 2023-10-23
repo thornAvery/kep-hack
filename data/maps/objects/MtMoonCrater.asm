6496a46d1dbddfe6f3e418ccff42854abb083de3
Llinos Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2023-10-23 13:45:51 +0100

    Beautify Mt. Moon Crater
    I made Mt. Moon Crater when KEP's ROM Hack was just starting, and in retrospect, it looked horrible. Lots of random things all over the place, not very layered, it really just looked like a normal route. Here, I've attempted to layer the crater more cleanly, added a "Zero Lab," and all-around improved the tree placement. Hopefully, this makes it more enjoyable to explore.

--- data/maps/objects/MtMoonCrater.asm
+++ data/maps/objects/MtMoonCrater.asm
@@ -9,7 +9,7 @@ MtMoonCrater_Object:
 
 	def_object_events
 	object_event 33, 17, SPRITE_FOSSIL, STAY, NONE, 1, DOME_FOSSIL
-	object_event 25, 21, SPRITE_FOSSIL, STAY, NONE, 2, WING_FOSSIL
+	object_event 31, 32, SPRITE_FOSSIL, STAY, NONE, 2, WING_FOSSIL
 	object_event 33, 16, SPRITE_FOSSIL, STAY, NONE, 3, HELIX_FOSSIL
 	object_event 45, 24, SPRITE_POKE_BALL, STAY, NONE, 4, MAX_ETHER
 
