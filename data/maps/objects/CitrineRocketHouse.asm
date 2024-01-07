573f9083c0fae753b2d7bc72ae00cdcabcf3f582
Martha Schilling <harleyrayquaza@gmail.com>
2024-01-07 18:34:07 +0000

    All TMs are now renewable
    Added the TMs that weren't already available in either Celadon or Citrine to Citrine.

--- data/maps/objects/CitrineRocketHouse.asm
+++ data/maps/objects/CitrineRocketHouse.asm
@@ -10,10 +10,12 @@ CitrineRocketHouse_Object:
 	def_object_events
 	object_event  4,  1, SPRITE_ROCKET, STAY, DOWN, 1 ; Mart Guy
 	object_event  5,  1, SPRITE_ROCKET, STAY, DOWN, 2 ; Mart Guy
-	object_event  1,  6, SPRITE_ROCKET, STAY, RIGHT, 3 ; person
-	object_event  9,  3, SPRITE_ROCKET, STAY, UP, 4 ; person
-	object_event  9,  8, SPRITE_CAT, STAY, UP, 5 ; Meowth
-	object_event  9,  5, SPRITE_JAMES, STAY, DOWN, 6 ; James
-	object_event  7,  6, SPRITE_JESSIE, STAY, RIGHT, 7 ; Jessie
+	object_event  6,  1, SPRITE_ROCKET, STAY, DOWN, 3 ; Mart Guy
+	object_event  7,  1, SPRITE_ROCKET, STAY, DOWN, 4 ; Mart Guy
+	object_event  1,  6, SPRITE_ROCKET, STAY, RIGHT, 5 ; person
+	object_event  9,  3, SPRITE_ROCKET, STAY, UP, 6 ; person
+	object_event  9,  8, SPRITE_CAT, STAY, UP, 7 ; Meowth
+	object_event  9,  5, SPRITE_JAMES, STAY, DOWN, 8 ; James
+	object_event  7,  6, SPRITE_JESSIE, STAY, RIGHT, 9 ; Jessie
 
 	def_warps_to CITRINE_ROCKET_HOUSE
