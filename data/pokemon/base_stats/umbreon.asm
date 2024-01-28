cc3640e6c7a9d74f1ed901b7d25d6d84c3886f16
Martha Schilling <harleyrayquaza@gmail.com>
2024-01-28 20:02:55 +0000

    Sorry, Umbreon fans
    It still gets to keep Psychic, but it's a tradeback move rather than by TM. Mainly citing balance reasons and also to keep the Eevees more consistent with regards to TM learnsets.

--- data/pokemon/base_stats/umbreon.asm
+++ data/pokemon/base_stats/umbreon.asm
@@ -15,8 +15,8 @@
 
 	; tm/hm learnset
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   \
-	     RAGE,         PSYCHIC_M,    MIMIC,        DOUBLE_TEAM,  REFLECT,      \
-	     BIDE,         SWIFT,        SKULL_BASH,   REST,         SUBSTITUTE
+	     RAGE,         MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         \
+	     SWIFT,        SKULL_BASH,   REST,         SUBSTITUTE
 	; end
 
 	db BANK(UmbreonPicFront)
