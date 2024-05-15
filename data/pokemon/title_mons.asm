2c6fc430112e0f39e91645c6b05a26b3f51b67eb
Martha Schilling <harleyrayquaza@gmail.com>
2024-05-15 20:46:36 +0100

    Title screen + Diploma improvements
    - An interesting thing I decided to implement here; every time the title screen gets loaded, it loads a random choice between Red and Green, as well as Scream Tail and Sandy Shocks. This effectively gives 4 different starting title screens.
    
    - Made the EXPN. PAK text on the title screen more centered.
    
    - Green now appears on the diploma instead of Red if you're playing as her.

--- data/pokemon/title_mons.asm
+++ data/pokemon/title_mons.asm
@@ -1,5 +1,7 @@
 TitleMons:
 ; mons on the title screen are randomly chosen from here
+	db SCREAM_TAIL  ; Mascots
+	db SANDY_SHOCKS
 	db TOTARTLE     ; New Starters
 	db GOROCHU
 	db SYLVEON
@@ -9,10 +11,8 @@ TitleMons:
 	db BELLIGNAN	; GS Betas
 	db LUXWAN
 	db PORYGON2     ; Post-Gen 1 Evos
-	db MAGNEZONE
-	db ANNIHILAPE
-	db SCREAM_TAIL  ; LGPE and SV
-	db WUGTRIO
-	db MELTAN
-	db EXEGGUTOR_A  ; Regional Variants
+	db TANGROWTH
+	db ANNIHILAPE	
+	db EXEGGUTOR_A  ; Regional Variants & Convergents
 	db TAUROS_PB
+	db WUGTRIO
